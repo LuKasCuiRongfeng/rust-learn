05e54f409c5a7c80d2e6ded4d8f4826c62185d36
cuirongfeng <cuirongfeng@aquilatech.ai>
2023-06-15 13:42:21 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..100);
+
+    // println!("your secret_numb is: {secret_number}");
+
+    loop {
+        println!("Please input your gusess");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please input a number");
+                continue;
+            },
+        };
+
+        println!("your guess: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+            Ordering::Greater => println!("too big"),
+        }
+    }
+
+    let _ = Command::new("cmd.exe").arg("/c").arg("pause").status();
 }
